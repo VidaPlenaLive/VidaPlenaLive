7e2953ad809ac1a9096f171ab49b9124dca9b00a
VidaPlenaLive <68176800+VidaPlenaLive@users.noreply.github.com>
2020-10-21 00:02:35 -0400

    Add files via upload

---
+++ Alumbramiento/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    const elementosCarousel=document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel,{
+
+        duration:500,
+        dist:-80,
+        shift:5,
+        padding:5,
+        numVisible:3,
+        indicators:true,
+        noWrap:true
+    });
+});
